54d8321758676b3213eb1d2721ab7adde2515452
aftuit <farruxaktamov6@gmail.com>
2021-07-12 20:30:18 +0500

    project created

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 3,
+        // stagePadding: 100,
+        // margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            500: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+})
+
+
+
+const searchFocus = document.getElementById('search-focus');
+const keys = [
+    { keyCode: 'AltLeft', isTriggered: false },
+    { keyCode: 'ControlLeft', isTriggered: false },
+];
+
+window.addEventListener('keydown', (e) => {
+    keys.forEach((obj) => {
+        if (obj.keyCode === e.code) {
+            obj.isTriggered = true;
+        }
+    });
+
+    const shortcutTriggered = keys.filter((obj) => obj.isTriggered).length === keys.length;
+
+    if (shortcutTriggered) {
+        searchFocus.focus();
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    keys.forEach((obj) => {
+        if (obj.keyCode === e.code) {
+            obj.isTriggered = false;
+        }
+    });
+});
